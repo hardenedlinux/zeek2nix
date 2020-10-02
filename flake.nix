818aafc387f76b983810798c9830d8da2cf13cad
GTrunSec <gtrunsec@hardenedlinux.org>
2020-10-02 04:29:58 -0700

    flakes: add TLS and current versions to zeek

--- flake.nix
+++ flake.nix
@@ -4,25 +4,57 @@
   inputs =  {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/302ef60620d277fc87a8aa58c5c561b62c925651";
+    zeek-plugin-ikev2 = { url = "github:ukncsc/zeek-plugin-ikev2/master"; flake = false;};
+    zeek-tls = { url = "https://download.zeek.org/zeek-3.0.10.tar.gz"; flake = false;};
+    zeek-current = { url = "https://download.zeek.org/zeek-3.2.1.tar.gz"; flake = false;};
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-            ];
-          };
-        in
-        rec {
-          packages = flake-utils.lib.flattenTree {
-            zeek = pkgs.callPackage ./. {};
-          };
-            devShell = import ./shell.nix { inherit pkgs; zeek = packages.zeek;};
-            defaultPackage = packages.zeek;
-            apps.hello = flake-utils.lib.mkApp { drv = packages.zeek; };
-          }
-      )
-    );
+
+
+  outputs = inputs: with builtins;
+    let
+      flakeLock = inputs.nixpkgs.lib.importJSON ./flake.lock;
+      loadInput = { locked, ... }:
+        builtins.fetchTarball {
+          url = locked.url;
+          sha256 = locked.narHash;
+        };
+    in
+      (inputs.flake-utils.lib.eachDefaultSystem
+        (system:
+          let
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                (import ./overlay.nix)
+              ];
+            };
+          in
+            rec {
+
+              zeekCurrent = pkgs.zeek.overrideAttrs(oldAttrs: {
+                version = "3.2.1";
+                src = builtins.fetchTarball {
+                  url = flakeLock.nodes.zeek-current.locked.url;
+                  sha256 = flakeLock.nodes.zeek-current.locked.narHash;
+                };
+              });
+
+              zeekTLS = pkgs.zeek.overrideAttrs(oldAttrs: {
+                version = "3.0.10";
+                src = builtins.fetchTarball {
+                  url = flakeLock.nodes.zeek-tls.locked.url;
+                  sha256 = flakeLock.nodes.zeek-tls.locked.narHash;
+                };
+              });
+              
+              packages = inputs.flake-utils.lib.flattenTree {
+                inherit zeekCurrent zeekTLS;
+              };
+              
+              devShell = import ./shell.nix { inherit pkgs zeekCurrent zeekTLS;};
+              defaultPackage = packages.zeekTLS;
+              apps.zeek = flake-utils.lib.mkApp { drv = packages.zeekCurrent; };
+            }
+        )
+      );
 }
--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 { pkgs ? import <nixpkgs> {}
-, zeek
+, zeekCurrent
+, zeekTLS
 }:
 
 with pkgs;
 
 mkShell {
   buildInputs = [
-    zeek
+    zeekCurrent
   ];
 }
