c7668f020854fb3589fd0c797a78eeac2bb62b69
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-16 18:48:22 -0800

    replace: update Scripts

--- nix/script.nix
+++ nix/script.nix
@@ -1,10 +1,11 @@
 {coreutils}:
 ''
-   sed -i 's|/bin/mv|${coreutils}/bin/mv|' scripts/base/frameworks/logging/writers/ascii.zeek
-   sed -i 's|/bin/mv|${coreutils}/bin/mv|' scripts/policy/misc/trim-trace-file.zeek
+   sed -i 's|/bin/rm|${coreutils}/bin/rm|' scripts/policy/misc/trim-trace-file.zeek
+   sed -i 's|/bin/rm|${coreutils}/bin/rm|' scripts/base/frameworks/notice/main.zeek
+   sed -i 's|/bin/rm|${coreutils}/bin/rm|' scripts/base/frameworks/logging/postprocessors/sftp.zeek
+
    sed -i 's|/bin/cat|${coreutils}/bin/cat|' scripts/base/frameworks/notice/actions/pp-alarms.zeek
    sed -i 's|/bin/cat|${coreutils}/bin/cat|' scripts/base/frameworks/notice/main.zeek
-
    substituteInPlace zeek-config.in --subst-var ZEEK_DIST
 
    export ZEEK_SRC=$(pwd)
