0e8d6582ea935538e8127d69e6c15cdd38122ea8
GTrunSec <gtrunsec@hardenedlinux.org>
2021-03-16 21:59:11 -0700

    comment maintainers list

--- nix/default.nix
+++ nix/default.nix
@@ -118,7 +118,7 @@ stdenv.mkDerivation rec {
     description = "Powerful network analysis framework much different from a typical IDS";
     homepage = "https://www.zeek.org";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ gtrunsec ];
+    #maintainers = with maintainers; [ gtrunsec ];
     platforms = platforms.unix;
   };
 }
