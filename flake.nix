20a01cae6f0e3d7b5ce02f693f53d816c2950481
GTrunSec <gtrunsec@hardenedlinux.org>
2022-01-03 18:01:35 -0800

    fix flake-registry

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   nixConfig.extra-substituters = "https://zeek.cachix.org";
   nixConfig.extra-trusted-public-keys = "zeek.cachix.org-1:Jv0hB/P5eF7RQUZgSQiVqzqzgweP29YIwpSiukGlDWQ=";
   nixConfig = {
-    #flake-registry = "https://github.com/hardenedlinux/flake-registry/raw/main/flake-registry.json";
-    flake-registry = "/home/gtrun/ghq/github.com/hardenedlinux/flake-registry/flake-registry.json";
+    flake-registry = "https://github.com/hardenedlinux/flake-registry/raw/main/flake-registry.json";
+    #flake-registry = "/home/gtrun/ghq/github.com/hardenedlinux/flake-registry/flake-registry.json";
   };
 
   inputs = {
