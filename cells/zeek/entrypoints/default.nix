6ca35f6cb826ca9ae6be2cef3abe12c8dc994fbe
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-14 13:25:39 -0700

    fix spicy-check

--- cells/zeek/entrypoints/default.nix
+++ cells/zeek/entrypoints/default.nix
@@ -1,5 +1,16 @@
 {
   inputs,
   cell,
-}: {
+}: let
+  inherit (inputs.cells-lab._writers.library) writeShellApplication;
+in {
+  spicyCheck = writeShellApplication {
+    name = "spicyCheck";
+    runtimeInputs = [cell.packages.zeek-release];
+    text = ''
+      cd "$PRJ_ROOT"/cells/zeek/overlays/nixos/scripts
+      zeek -NN Zeek::Spicy
+      spicyz -D zeek my-http.spicy my-http.evt -o my-http.hlto
+    '';
+  };
 }
